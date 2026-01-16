87508beadc92331a72d638f7be16573d65502d7e
Yoonseok Kang <kangcl1609@gmail.com>
2026-01-17 07:09:00 +0900

    Format KafkaConsumerConfig for improved readability and organization

--- consumers/ingest/src/main/java/onlog/consumer/ingest/KafkaConsumerConfig.java
+++ consumers/ingest/src/main/java/onlog/consumer/ingest/KafkaConsumerConfig.java
@@ -1,3 +1,4 @@
+
 package onlog.consumer.ingest;
 
 import onlog.common.serde.CanonicalEventSerde;
@@ -10,24 +11,41 @@ import java.util.Properties;
 
 public class KafkaConsumerConfig {
 
-    public static Properties base(String groupId) {
+        public static Properties base(String groupId) {
 
         Properties p = new Properties();
 
-        p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                System.getenv("KAFKA_BOOTSTRAP"));
+        p.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                System.getenv("KAFKA_BOOTSTRAP")
+        );
+
+        // =========================
+        // MSK IAM (🔥 필수 🔥)
+        // =========================
+        p.put("security.protocol", "SASL_SSL");
+        p.put("sasl.mechanism", "AWS_MSK_IAM");
+        p.put(
+                "sasl.jaas.config",
+                "software.amazon.msk.auth.iam.IAMLoginModule required;"
+        );
+        p.put(
+                "sasl.client.callback.handler.class",
+                "software.amazon.msk.auth.iam.IAMClientCallbackHandler"
+        );
 
         p.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         p.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-
         p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        p.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-
-        p.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
+        p.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class
+        );
+        p.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class
+        );
 
         return p;
-    }
-}
+        }
